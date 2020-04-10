b8ecf3b5a536cf9b957c195e13e50371b8f99031
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-04-10 20:23:15 +0530

    Create TheMaximumSubarray.cpp

---
+++ TheMaximumSubarray.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the maxSubarray function below.
+vector<int> maxSubarray(vector<int> a) {
+int sum=0;
+unordered_map<int,int>m;
+int i,n=a.size(),val=0,max_sum=INT_MIN;
+vector<int>v;
+for(i=0;i<n;++i)
+{
+    val=max(a[i],a[i]+val);
+    max_sum=max(max_sum,val);
+}
+v.emplace_back(max_sum);
+sort(a.begin(),a.end(),greater<int>());
+max_sum=0;
+i=0;
+if(a[i]<0)v.emplace_back(a[i]);
+else{
+    while(a[i]>0)
+    {
+        max_sum+=a[i];
+        ++i;
+    }
+    v.emplace_back(max_sum);
+}
+return v;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ofstream fout(getenv("OUTPUT_PATH"));
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector<string> arr_temp = split_string(arr_temp_temp);
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
+        }
+
+        vector<int> result = maxSubarray(arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
